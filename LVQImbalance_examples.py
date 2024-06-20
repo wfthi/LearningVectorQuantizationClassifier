6ed214332ce3fedca3badcce2774e86b2d8ddfe1
Wing-Fai Thi <wft@mpe.mpg.de>
2024-06-20 13:29:35 +0200

    Better documentation2

--- LVQImbalance_examples.py
+++ LVQImbalance_examples.py
@@ -107,7 +107,7 @@ X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.1,
                                                     stratify=y,
                                                     random_state=42)
 
-# SMOTE + tree-LVQ
+# SMOTE + tree-LVQ (kmeans-LVQs)
 sm = SMOTE(sampling_strategy=0.8, random_state=1235)
 X_res, y_res = sm.fit_resample(X_train, y_train)
 n_prototypes = 5
